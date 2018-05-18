799c555e11b9512d54e02f89c1a07083beee06ba
tenneco <36246292+tenneco@users.noreply.github.com>
2018-05-18 20:29:25 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -79,7 +79,7 @@ def cadastro_vestibulares(request):
 )
 
 
-def inscrição_de_candidatos(request):
+def inscricao_de_candidatos(request):
     assert isinstance(request, HttpRequest)
     return render(
     request,
